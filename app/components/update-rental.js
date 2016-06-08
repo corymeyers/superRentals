8d0a6f3c321f8391bbb276ef197501547b66c0bf
cory <whippoorwill.in.flight@gmail.com>
2016-06-08 15:34:14 -0700

    Firebase now updates

--- app/components/update-rental.js
+++ app/components/update-rental.js
@@ -6,12 +6,38 @@ export default Ember.Component.extend({
     updateRentalForm() {
       this.set('updateRentalForm', true);
     },
+
     update(rental) {
+      var ownerFromForm;
+      if (typeof this.get('owner') !== "undefined") { //if the owner exists
+        ownerFromForm = this.get('owner');
+      } else {
+        ownerFromForm = "";
+      }
+      var cityFromForm;
+      if (typeof this.get('city') !== "undefined") {
+        cityFromForm = this.get('city');
+      } else {
+        cityFromForm = "";
+      }
+      var typeFromForm;
+      if (typeof this.get('type') !== "undefined") {
+        typeFromForm = this.get('type');
+      } else {
+        typeFromForm = "";
+      }
+      var imageFromForm;
+      if (typeof this.get('image') !== "undefined") {
+        imageFromForm = this.get('image');
+      } else {
+        imageFromForm = "http://resource.mmgn.com/Gallery/full/A-dead-Link-404-1041428.jpg";
+      }
+
       var params = {
-        owner: this.get('owner'),
-        city: this.get('city'),
-        type: this.get('type'),
-        image: this.get('image'),
+        owner: ownerFromForm,
+        city: cityFromForm,
+        type: typeFromForm,
+        image: imageFromForm,
         bedrooms: this.get('bedrooms'),
       };
       this.set('updateRentalForm', false);
