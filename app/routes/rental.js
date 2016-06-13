802f341c9873551a6b07a0370a1bfcb6b9edbadb
cory <whippoorwill.in.flight@gmail.com>
2016-06-13 10:18:20 -0700

    update embeefire and firebase

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -13,6 +13,10 @@ export default Ember.Route.extend({
       });
       rental.save();
       this.transitionTo('index');
+    },
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
     }
   }
 });
--- config/environment.js
+++ config/environment.js
@@ -1,13 +1,20 @@
-/* jshint node: true */
+
 
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'super-rentals',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://super-rentals-28ea4.firebaseio.com/',
+    firebase: {
+      apiKey: "AIzaSyBud50zzlkTMTLvVW4Mg1EjL1v9XbeZ0NI",
+      authDomain: "super-rentals-28ea4.firebaseapp.com",
+      databaseURL: "https://super-rentals-28ea4.firebaseio.com",
+      storageBucket: "super-rentals-28ea4.appspot.com",
+    },
+
     baseURL: '/',
     locationType: 'auto',
+
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
